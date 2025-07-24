cd9f11449ad9da46d48b91d32ad84b75cb21177e
srihariiiiiii <srih50680@gmail.com>
2025-07-24 15:49:05 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -1,43 +1,42 @@
-//array
-//let number=[1,2,"three",true];
+array
+let number=[1,2,"three",true];
 
-//let names=new Array("john","james");
+let names=new Array("john","james");
+console.log(number[2]);
+console.log(names.length);
+filter()-it filterd the result arguments
 
-//console.log(number[2]);
-//console.log(names.length);
-//filter()-it filterd the result arguments
-
-//let result=number.filter(number => number[1]);
-//console.log(result);
+let result=number.filter(number => number[1]);
+console.log(result);
 //slice - delete the element based on index value
 //slice - changes the original array by
 //adding/removeing items at a specific index
 
-//let number=[1,2,3,4,5];
-//number.splice(1,2,"hello");
-//console.log(number)
+let number=[1,2,3,4,5];
+number.splice(1,2,"hello");
+console.log(number)
 
-//object
+object
 
-//let obj={
-  //  name:"saiiiiiiiiiiiiiii",
-    //totalmark:"45",
-    //nested obj
-   // subject:{
-       // python:"100",
-      //  DS:"98",
+let obj={
+   name:"saiiiiiiiiiiiiiii",
+    totalmark:"45",
+    nested obj
+    subject:{
+        python:"100",
+        DS:"98",
 
-  //  avg:function(){
-       // let sum=parseInt(this.subject.DS)+ parseInt(this.subject.python);
-       // return sum;
-  //  }
+    avg:function(){
+        let sum=parseInt(this.subject.DS)+ parseInt(this.subject.python);
+        return sum;
+   }
 
-//}
-//console.log(obj.totalmark);
-//console.log(obj["name"]);
-//console.log(obj.subject.python);
-//console.log(obj.subject["DS"]);
-//console.log(obj.avg());
+}
+console.log(obj.totalmark);
+console.log(obj["name"]);
+console.log(obj.subject.python);
+console.log(obj.subject["DS"]);
+console.log(obj.avg());
 
 const person={
     name:"saii",
